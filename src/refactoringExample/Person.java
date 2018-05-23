b984596b9d9f9176e5f49d8000a9840188b71af1
1791330 <1791330@LI-S064-0T17>
2018-05-23 08:44:18 -0400

    Class creation

---
+++ src/refactoringExample/Person.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package refactoringExample;
+
+/**
+ *
+ * @author 1791330
+ */
+public class Person {
+    int age;
+    
+    public void study(){
+        System.out.println("I study");
+    }
+}
