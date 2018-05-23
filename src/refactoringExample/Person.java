ab1cf8eb966e6bcde06ffaf0033d8a1e76f998dd
1791330 <1791330@LI-S064-0T17>
2018-05-23 09:47:34 -0400

    Person refactored

--- src/refactoringExample/Person.java
+++ src/refactoringExample/Person.java
@@ -12,9 +12,66 @@ import java.util.Date;
  * @author 1791330
  */
 public class Person {
-    int age;
+    
+    private int age;
     Date dateBirth;
     String firstName;
     String lastName;
+
+    /**
+     * @return the age
+     */
+    public int getAge() {
+        return age;
+    }
+
+    /**
+     * @param age the age to set
+     */
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    /**
+     * @return the dateBirth
+     */
+    public Date getDateBirth() {
+        return dateBirth;
+    }
+
+    /**
+     * @param dateBirth the dateBirth to set
+     */
+    public void setDateBirth(Date dateBirth) {
+        this.dateBirth = dateBirth;
+    }
+
+    /**
+     * @return the firstName
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     * @param firstName the firstName to set
+     */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    /**
+     * @return the lastName
+     */
+    public String getLastName() {
+        return lastName;
+    }
+
+    /**
+     * @param lastName the lastName to set
+     */
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
     
 }
