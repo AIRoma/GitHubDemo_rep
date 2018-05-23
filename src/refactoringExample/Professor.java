a572c2b87e3831779ea33b7ba511cf5a781d3f41
1791330 <1791330@LI-S064-0T17>
2018-05-23 08:54:36 -0400

    Class Professor created

--- src/refactoringExample/Professor.java
+++ src/refactoringExample/Professor.java
@@ -5,10 +5,25 @@
  */
 package refactoringExample;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author 1791330
  */
-public class Professor {
+public class Professor extends Person{
+    List<Student> students = new ArrayList<>();
     
+    String photoUrl;
+    
+    public Professor(String lastName, String firstName){
+        String employeeID = lastName.toUpperCase().trim().substring(1, 3) + firstName.toUpperCase().trim().substring(1, 3);
+        
+        System.out.println("New Professor: " + employeeID);
+    }
+    
+    public void setPhotoUrl(String photoName){
+        this.photoUrl = "/photos/professor/mad/" + photoName;
+    }
 }
