cf39e29b41c88a5592b58c095164c1492e6bc629
1791330 <1791330@LI-S064-0T17>
2018-05-23 09:03:39 -0400

    Class Student finished

--- src/refactoringExample/Student.java
+++ src/refactoringExample/Student.java
@@ -5,10 +5,43 @@
  */
 package refactoringExample;
 
+import java.util.Date;
+
 /**
  *
  * @author 1791330
  */
-public class Student {
+public class Student extends Person{
+    String student_id;
+    String lastName;
+    String firstName;
+    Date dateBirth;
+
+    public Student(String student_id, String lastName, String firstName, Date dateBirth) {
+        this.student_id = student_id;
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.dateBirth = dateBirth;
+    }
     
+    public String createStudentId(String lName, String fName, Date date){
+        String end = concatDate(date);
+        return lName.substring(0, 3) + fName.charAt(0) + end;
+    }
+
+    private String concatDate(Date date) {
+        return date.getYear() + "-" + date.getMonth();
+    }
+
+    public String getStudent_id() {
+        return student_id;
+    }
+
+    public void setStudent_id(String student_id) {
+        this.student_id = student_id;
+    }
+    
+    public void isSupervisedBy(Professor professor){
+        professor.students.add(this);
+    }
 }
